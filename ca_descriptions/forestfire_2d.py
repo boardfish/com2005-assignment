3b15e0e15f82ffc4412ea0d56e89bb2c60dfbc65
Jack Barradell-Johns <jirbarradell-johns1@sheffield.ac.uk>
2018-03-07 10:16:08 +0000

    Small changes

--- ca_descriptions/forestfire_2d.py
+++ ca_descriptions/forestfire_2d.py
@@ -60,9 +60,8 @@ def setup(args):
     config_path = args[0]
     config = utils.load(config_path)
     # ---THE CA MUST BE RELOADED IN THE GUI IF ANY OF THE BELOW ARE CHANGED---
-    config.title = "Conway's game of life"
+    config.title = "Forest Fire Simulation"
     config.dimensions = 2
-    config.states = (0, 1)
     # ------------------------------------------------------------------------
 
     # ---- Override the defaults below (these may be changed at anytime) ----
@@ -70,7 +69,6 @@ def setup(args):
     # config.state_colors = [(0,0,0),(1,1,1)]
     config.states = (0,1,2,3,4,5)
     config.state_colors = [(0.6,0.6,0.4),(0.25,0.6,0.3),(0.5,0.5,0.5),(0.1,0.1,0.9),(0.75,0.4,0.3),(0,0,0)]
-
     config.num_generations = 500
     config.grid_dims = (50,50)
     config.wrap = False    
